d741d20bdb5c1ce2ec19acfd96800d34884a15cc
akshay8149 <44441380+akshay8149@users.noreply.github.com>
2018-10-25 16:36:21 +0530

    Angular.pipepage.ts

---
+++ Angular.piepage.ts
@@ -0,0 +1,23 @@
+import { Component,OnInit,Input } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {pipepage} from '../pipe';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor() {
+
+  }
+
+
+EnterText:any
+
+
+
+
+}
